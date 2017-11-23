8961b452695bbd1b96599d07742bc82271504fc4
Gloner <antoine.gloner@orange.frgit config --global user.name Antoinegit user.email antoine.gloner@orange.fr>
2017-11-23 16:53:02 +0100

    ajout du css et js

---
+++ JS/app.js
@@ -0,0 +1,121 @@
+function suppr()
+{
+	var valeur = document.getElementById("resultat").value;
+	var longueur = valeur.length;
+	var fin = longueur - 1;
+
+	valeur = valeur.substring(0, fin);
+	document.getElementById("resultat").value = valeur;
+}
+
+function un ()
+{
+
+	var x = document.getElementById("un").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function deux ()
+{
+
+	var x = document.getElementById("deux").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function trois()
+{
+	var x = document.getElementById("trois").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function quatre()
+{
+	var x = document.getElementById("quatre").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+function cinq()
+{
+	var x = document.getElementById("cinq").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function six()
+{
+	var x = document.getElementById("six").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function sept()
+{
+	var x = document.getElementById("sept").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function huit()
+{
+	var x = document.getElementById("huit").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;	
+}
+
+function neuf()
+{
+	var x = document.getElementById("neuf").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+
+}
+
+function zero()
+{
+	var x = document.getElementById("zero").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+
+}
+
+
+function egal()
+{
+	var x = document.getElementById("egal").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function plus()
+{
+	var x = document.getElementById("plus").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+
+	
+
+}
+
+function moins()
+{
+	var x = document.getElementById("moins").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function multipli()
+{
+	var x = document.getElementById("multipli").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
+
+function division()
+{
+	var x = document.getElementById("division").value;
+	var valeur = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = valeur + x;
+}
